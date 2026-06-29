b5549a13d6c82d4f058e0c7a310e5bd14fe20d26
lys861205 <447427053@qq.com>
2026-06-29 15:06:52 +0800

    fix: move database push to docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running database migrations..."
-cd /app && npx drizzle-kit migrate
+cd /app && npx drizzle-kit push --force
 
 echo "Starting services..."
 exec /usr/bin/supervisord -c /etc/supervisor/conf.d/growthpilot.conf
